9107c417c3ac09b75034d9e2002fa784a921b769
Drew Loomer <drew.loomer@projekt202.com>
2020-01-12 13:16:20 -0600

    Fixes broken tests.

--- src/util/scroll-position.spec.tsx
+++ src/util/scroll-position.spec.tsx
@@ -1,36 +1,11 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { renderHook, act } from '@testing-library/react-hooks';
-import {
-  useFixedOnScrollBack,
-  useScrollPosition,
-  useRefDimensions,
-  useScrolledBack
-} from './scroll-position';
+import { useFixedOnScrollBack } from './scroll-position';
 
 const TestComponent = () => <nav />;
 
 describe('Track scroll position', () => {
-  it('should return the scroll position', () => {
-    const { result } = renderHook(() => useScrollPosition());
-    expect(result.current).toEqual([0, 0]);
-    // @todo: test this with scrolling
-  });
-
-  it('should return the dimensions of a reference object', () => {
-    const el = render(<TestComponent />);
-    const { result } = renderHook(() =>
-      useRefDimensions({ current: el.baseElement })
-    );
-    expect(result.current).toEqual([0, 0]);
-  });
-
-  it('should return true when the user has scrolled back', () => {
-    const { result } = renderHook(() => useScrolledBack());
-    expect(result.current).toBe(null);
-    // @todo: test this with scrolling
-  });
-
   it('should return fixed as true when the user is scrolling backwards', () => {
     const el = render(<TestComponent />);
     const { result } = renderHook(() =>
--- src/util/scroll-position.ts
+++ src/util/scroll-position.ts
@@ -1,81 +1,11 @@
 import {
   useState,
-  useLayoutEffect,
   MutableRefObject,
   useEffect,
-  DependencyList,
   Dispatch,
   SetStateAction
 } from 'react';
 import { offset } from './element-offset';
-
-/**
- * Track the current scroll position
- */
-export const useScrollPosition = (
-  onChange: (x: number, y: number) => void = () => {},
-  deps: DependencyList = []
-) => {
-  const [[x, y], setPosition] =
-    typeof window !== 'undefined'
-      ? useState([window.scrollX, window.scrollY])
-      : useState([0, 0]);
-  const check = (): void => {
-    onChange(window.scrollX, window.scrollY);
-    setPosition([window.scrollX, window.scrollY]);
-  };
-  useLayoutEffect(() => {
-    document.addEventListener('scroll', check);
-    return () => {
-      document.removeEventListener('scroll', check);
-    };
-  }, deps);
-  return [x, y];
-};
-
-/**
- * Track the dimensions of a reference element
- */
-export const useRefDimensions = (
-  ref: MutableRefObject<HTMLElement>,
-  deps: DependencyList = []
-) => {
-  const [[width, height], setDimensions] = useState([0, 0]);
-  useLayoutEffect(() => {
-    setDimensions([ref.current.clientWidth, ref.current.clientHeight]);
-  }, deps);
-  return [width, height];
-};
-
-/**
- * Monitors scrolling on the window to see if the user has scrolled backwards
- * @todo: should allow for a couple pixels of downward movement after scrolled back
- */
-export const useScrolledBack = (deps: DependencyList = []) => {
-  const scrollHistory: number[] = [];
-  // const [scrollHistory, setScrollHistory] = useState<number[]>([]);
-  // const [isScrolledBack, setIsScrolledBack] = useState<boolean>();
-  // useEffect(() => {
-  //   setScrollHistory(scrollHistory.concat([y]).slice(-5));
-  //   setIsScrolledBack(
-  //     scrollHistory.reverse().reduce((acc: boolean, cur, i, arr) => {
-  //       const next = arr[i + 1];
-  //       if (next === undefined) return acc;
-  //       return acc !== false && cur < next;
-  //     }, null)
-  //   );
-  // }, [y, ...deps]);
-  return [
-    (y: number) => {
-      // scrollHistory.push(y);
-      return false; /*scrollHistory.reverse().reduce((acc: boolean, cur, i, arr) => {
-        const next = arr[i + 1];
-        if (next === undefined) return acc;
-        return acc !== false && cur < next;
-      }, null);*/
-    }
-  ];
-};
 
 enum ScrollDirection {
   Up = 0,
